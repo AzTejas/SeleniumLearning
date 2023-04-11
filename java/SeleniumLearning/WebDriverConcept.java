f0f94e9cfb378477b6aeac345b2bede72a3290d9
Tejas Patel <124738374+AzTejas@users.noreply.github.com>
2023-04-11 22:52:26 +0100

    Update WebDriverConcept.java

--- java/SeleniumLearning/WebDriverConcept.java
+++ java/SeleniumLearning/WebDriverConcept.java
@@ -8,11 +8,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class WebDriverConcept {
     public static void main(String[] args) {
-        /*
-        In the market, there is a utility library to launch a browser without any executable file.
-        To overcome setting up driver part - There is an API available Bonigarcia
-        API saying use me I will do everything for you.
-         */
+       
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
